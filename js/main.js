abcc17f2339eb1b1c535485bbc129724d0927f0f
jessicacvetkovska <jessicacvetkovska@gmail.com>
2026-02-02 01:03:29 -0500

    unzipped data and data is loaded in console!

---
+++ js/main.js
@@ -0,0 +1,58 @@
+console.log("Hello world");
+let data_contraceptive, data_literacyrate;
+
+d3.csv('data/contraceptiveprevalence.csv')
+  .then(_data => {
+  	console.log('Data loading of contraceptive prevalence complete. Work with dataset.');
+  	data_contraceptive = _data;
+    console.log(data_contraceptive);
+
+    // //process the data - this is a forEach function.  You could also do a regular for loop.... 
+    // data.forEach(d => { //ARROW function - for each object in the array, pass it as a parameter to this function
+    //   	d.cost = +d.cost; // convert string 'cost' to number
+    //   	d.daysFromYrStart = computeDays(d.start); //note- I just created this field in each object in the array on the fly
+
+	// 			let tokens = d.start.split("-");
+  	// 		d.year = +tokens[0];
+
+  	// });
+
+  	// // Create an instance (for example in main.js)
+	// 	timelineCircles = new TimelineCircles({
+	// 		'parentElement': '#timeline',
+	// 		'containerHeight': 1100,
+	// 		'containerWidth': 1000
+	// 	}, data);
+})
+.catch(error => {
+    console.error('Error:');
+    console.log(error);
+});
+
+d3.csv('data/literacyrates.csv')
+  .then(_data => {
+  	console.log('Data loading of literacy rates complete. Work with dataset.');
+  	data_literacyrate = _data;
+    console.log(data_literacyrate);
+
+    // //process the data - this is a forEach function.  You could also do a regular for loop.... 
+    // data.forEach(d => { //ARROW function - for each object in the array, pass it as a parameter to this function
+    //   	d.cost = +d.cost; // convert string 'cost' to number
+    //   	d.daysFromYrStart = computeDays(d.start); //note- I just created this field in each object in the array on the fly
+
+	// 			let tokens = d.start.split("-");
+  	// 		d.year = +tokens[0];
+
+  	// });
+
+  	// // Create an instance (for example in main.js)
+	// 	timelineCircles = new TimelineCircles({
+	// 		'parentElement': '#timeline',
+	// 		'containerHeight': 1100,
+	// 		'containerWidth': 1000
+	// 	}, data);
+})
+.catch(error => {
+    console.error('Error:');
+    console.log(error);
+});
