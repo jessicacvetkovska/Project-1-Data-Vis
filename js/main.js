30279807e91489483dd87359a3cfbd094cc19e5c
jessicacvetkovska <jessicacvetkovska@gmail.com>
2026-02-03 02:06:18 -0500

    scatterplot created, scatterplot tooltip working, and more styling rules!

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 console.log("Hello world");
 let data_contraceptive, data_literacyrate;
 
-d3.csv('data/contraceptiveprevalence.csv')
+const contraceptivePromise = d3.csv('data/contraceptiveprevalence.csv')
   .then(_data => {
   	console.log('Data loading of contraceptive prevalence complete. Work with dataset.');
   	data_contraceptive = _data;
@@ -16,22 +16,24 @@ d3.csv('data/contraceptiveprevalence.csv')
 			latest[d.Entity] = {...d, Year: year};
 		}
   	});
-	const filteredData = Object.values(latest);
-	console.log(filteredData);
+	const contraceptiveFilteredData = Object.values(latest);
+	console.log(contraceptiveFilteredData);
 
   	// Create an instance (for example in main.js)
 	histogramcontraceptives = new HistogramContraceptives({
 		'parentElement': '#histogramcontraceptives',
 		'containerHeight': 500,
 		'containerWidth': 500
-	}, filteredData);
+	}, contraceptiveFilteredData);
+
+	return contraceptiveFilteredData;
 })
 .catch(error => {
     console.error('Error:');
     console.log(error);
 });
 
-d3.csv('data/literacyrates.csv')
+const literacyPromise = d3.csv('data/literacyrates.csv')
   .then(_data => {
   	console.log('Data loading of literacy rates complete. Work with dataset.');
   	data_literacyrate = _data;
@@ -46,17 +48,46 @@ d3.csv('data/literacyrates.csv')
 			latest[d.Entity] = {...d, Year: year};
 		}
   	});
-	const filteredData = Object.values(latest);
-	console.log(filteredData);
+	const litrateFilteredData = Object.values(latest);
+	console.log(litrateFilteredData);
 
   	// Create an instance (for example in main.js)
 	histogramliteracyrates = new HistogramLiteracyRates({
 		'parentElement': '#histogramliteracyrates',
 		'containerHeight': 500,
 		'containerWidth': 500
-	}, filteredData);
+	}, litrateFilteredData);
+
+	return litrateFilteredData;
 })
 .catch(error => {
     console.error('Error:');
     console.log(error);
 });
+
+
+Promise.all([contraceptivePromise, literacyPromise])
+  .then(([contraceptiveFilteredData, litrateFilteredData]) => {
+    // Combine datasets for scatterplot
+    const combinedData = contraceptiveFilteredData.map(d => {
+      const literacyMatch = litrateFilteredData.find(lit => lit.Entity === d.Entity);
+      if (literacyMatch) {
+        return {
+          Entity: d.Entity,
+          contraceptiveX: d.Prevalence,
+          litrateY: literacyMatch.LiteracyRate
+        };
+      }
+    }).filter(d => d !== undefined);
+
+    console.log('Combined data for scatterplot:', combinedData);
+
+    scatterplot = new Scatterplot({
+      'parentElement': '#scatterplot',
+      'containerHeight': 500,
+      'containerWidth': 500
+    }, combinedData);
+  })
+  .catch(error => {
+    console.error('Error combining datasets:', error);
+  });
